ae1a95f7a583478c9fbae8abee923a1345fad7e8
mxh <blf20822@126.com>
2018-11-21 18:52:26 +0800

    修改

--- src/main/java/com/newWebApp/controller/UserController.java
+++ src/main/java/com/newWebApp/controller/UserController.java
@@ -1,16 +1,11 @@
 package com.newWebApp.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import com.newWebApp.pojo.User;
 import com.newWebApp.service.IUserService;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,7 +20,7 @@ public class UserController {
     @ResponseBody
     public Map<Object , Object> getAnaloghistory(){
         Map<Object , Object> result = new HashMap<Object , Object>();
-        Integer userId = 1;
+        Integer userId = 123;
         User user = this.userService.selectUser(userId);
         result.put("test",user.getName());
         return result;
--- src/main/java/com/newWebApp/redisUtil/RedisConfig.java
+++ src/main/java/com/newWebApp/redisUtil/RedisConfig.java
@@ -1,18 +1,11 @@
 package com.newWebApp.redisUtil;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.*;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 /**
  * @author janti
@@ -22,104 +15,37 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableCaching
 public class RedisConfig extends CachingConfigurerSupport {
 
-    /**
-     * 选择redis作为默认缓存工具
-     * @param redisTemplate
-     * @return
-     */
-    @Bean
-    public CacheManager cacheManager(RedisTemplate redisTemplate) {
-        RedisCacheManager rcm = new RedisCacheManager(redisTemplate);
-        return rcm;
+    private volatile JedisConnectionFactory jedisConnectionFactory;
+    private volatile RedisTemplate<String, String> redisTemplate;
+    private volatile RedisCacheManager redisCacheManager;
+
+    public RedisConfig() {
+        super();
     }
 
     /**
-     * retemplate相关配置
-     * @param factory
-     * @return
-     */
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
-
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        // 配置连接工厂
-        template.setConnectionFactory(factory);
-
-        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
-        Jackson2JsonRedisSerializer jacksonSeial = new Jackson2JsonRedisSerializer(Object.class);
-
-        ObjectMapper om = new ObjectMapper();
-        // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
-        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        // 指定序列化输入的类型，类必须是非final修饰的，final修饰的类，比如String,Integer等会跑出异常
-        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        jacksonSeial.setObjectMapper(om);
-
-        // 值采用json序列化
-        template.setValueSerializer(jacksonSeial);
-        //使用StringRedisSerializer来序列化和反序列化redis的key值
-        template.setKeySerializer(new StringRedisSerializer());
-
-        // 设置hash key 和value序列化模式
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(jacksonSeial);
-        template.afterPropertiesSet();
-
-        return template;
-    }
-
-    /**
-     * 对hash类型的数据操作
+     * 带参数的构造方法 初始化所有的成员变量
      *
+     * @param jedisConnectionFactory
      * @param redisTemplate
-     * @return
+     * @param redisCacheManager
      */
-    @Bean
-    public HashOperations<String, String, Object> hashOperations(RedisTemplate<String, Object> redisTemplate) {
-        return redisTemplate.opsForHash();
+    public RedisConfig(JedisConnectionFactory jedisConnectionFactory, RedisTemplate<String, String> redisTemplate,
+                            RedisCacheManager redisCacheManager) {
+        this.jedisConnectionFactory = jedisConnectionFactory;
+        this.redisTemplate = redisTemplate;
+        this.redisCacheManager = redisCacheManager;
     }
 
-    /**
-     * 对redis字符串类型数据操作
-     *
-     * @param redisTemplate
-     * @return
-     */
-    @Bean
-    public ValueOperations<String, Object> valueOperations(RedisTemplate<String, Object> redisTemplate) {
-        return redisTemplate.opsForValue();
+    public JedisConnectionFactory getJedisConnecionFactory() {
+        return jedisConnectionFactory;
     }
 
-    /**
-     * 对链表类型的数据操作
-     *
-     * @param redisTemplate
-     * @return
-     */
-    @Bean
-    public ListOperations<String, Object> listOperations(RedisTemplate<String, Object> redisTemplate) {
-        return redisTemplate.opsForList();
+    public RedisTemplate<String, String> getRedisTemplate() {
+        return redisTemplate;
     }
 
-    /**
-     * 对无序集合类型的数据操作
-     *
-     * @param redisTemplate
-     * @return
-     */
-    @Bean
-    public SetOperations<String, Object> setOperations(RedisTemplate<String, Object> redisTemplate) {
-        return redisTemplate.opsForSet();
-    }
-
-    /**
-     * 对有序集合类型的数据操作
-     *
-     * @param redisTemplate
-     * @return
-     */
-    @Bean
-    public ZSetOperations<String, Object> zSetOperations(RedisTemplate<String, Object> redisTemplate) {
-        return redisTemplate.opsForZSet();
+    public RedisCacheManager getRedisCacheManager() {
+        return redisCacheManager;
     }
 }
